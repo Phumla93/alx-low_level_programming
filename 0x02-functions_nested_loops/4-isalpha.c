b27550d735d20888ca6b0bf4ed08530a78b73c07
Phumla93 <12035.mbazima@gmail.com>
2022-12-20 21:57:35 -0800

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - Return 1 if c is a letter. lowercase or uppercase
  * @c: unsigned int value to be compared with ASCII value
  * Return: 1 if its alphabet otherwise 0
- * */
+ */
 
 int _isalpha(int c)
 {
