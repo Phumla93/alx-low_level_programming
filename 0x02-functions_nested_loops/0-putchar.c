5dd6826a63846330c17344764e067dc5de180431
Phumla93 <12035.mbazima@gmail.com>
2023-01-03 14:18:38 -0800

    function that encodes a string using rot13.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *main - prints _putchar, followed by new line
- *
- *Return : Always 0 (Success)
- *
+ * main - Entry point
+ * Return : Always 0 (Success)
  */
+
 int main(void)
+
 {
 
 	_putchar('_');
